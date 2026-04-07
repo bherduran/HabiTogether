30b3fd72a74f43ff90e6d3a363c85318a9c5127a
Bilge Han Erduran <bilgehanerduran@gmail.com>
2026-04-07 15:58:31 +0300

    feat: expo-router setup with tab navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="progress" options={{ title: 'Progress' }} />
+      <Tabs.Screen name="focus" options={{ title: 'Focus' }} />
+    </Tabs>
+  );
+}
